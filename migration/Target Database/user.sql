29059d1dda168f4f39191d52d12d9b3793589021
mahery76 <109660691+mahery76@users.noreply.github.com>
2022-11-15 01:02:39 +0300

    swithing to oracle 21 movies

--- migration/Target Database/user.sql
+++ migration/Target Database/user.sql
@@ -1,21 +1,23 @@
 select * from dba_data_files;
+select tablespace_name from dba_tablespaces order by 1;
 
 CREATE TABLESPACE migration 
 datafile 'migration2.dbf'
 SIZE 2M;
 
-CREATE TEMPORARY TABLESPACE temp_migr
-TEMPFILE 'temp_migration2.dbf' SIZE 2M;
+-- CREATE TEMPORARY TABLESPACE temp_migr
+-- TEMPFILE 'temp_migration.dbf' SIZE 2M;
 
-SELECT pdb_name from dba_pdbs;
+-- SELECT pdb_name from dba_pdbs;
+
+
 alter session set container=XEPDB1;
 
 CREATE USER Mahery
 IDENTIFIED BY "test"
 DEFAULT TABLESPACE migration
 QUOTA 1M ON migration
-TEMPORARY TABLESPACE temp_migr
-QUOTA 5M ON system;
+QUOTA 5M ON system; 
 
 GRANT create session, create any table, alter any table to Mahery;
 
@@ -26,5 +28,10 @@ select USERNAME,ACCOUNT_STATUS,PASSWORD_VERSIONS from dba_users;
 -- Enter user-name: khristine/Password@Localhost/XEPDB1
 
 
+-- EXECUTE FILE IN RELATIVE PATH
 
 
+
+
+-- TRANSPARENT NETWORK SUBSTRATE
+-- SYSTEM IDENTIFICATION
